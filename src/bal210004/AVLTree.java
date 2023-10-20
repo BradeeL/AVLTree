61ca75f3950258f449aafe43b95f1aa1ccf6b7b1
Brady Lewis <lawlmomotx@gmail.com>
2023-10-20 15:51:10 -0500

    Implemented LL Rotations

--- src/bal210004/AVLTree.java
+++ src/bal210004/AVLTree.java
@@ -27,7 +27,49 @@ public class AVLTree<T extends Comparable<? super T>> extends BinarySearchTree<T
     @Override
     public boolean add(T x) {
         boolean retValue = super.add(new Entry<>(x,null,null));
+		find((Entry<T>)root,x);
+		Entry<T> current;
+		Entry<T> left;
+		Entry<T> right;
+		int leftHeight;
+		int rightHeight;
+		while(stack.peek()!=null){
+			current=(Entry<T>) stack.pop();
+			//Adjust Height
+			if(current.left!=null){
+				left=(Entry<T>) current.left;
+				leftHeight=left.height;
+			} else {
+				leftHeight=-1;
+			}
+			if(current.right!=null){
+				right=(Entry<T>) current.right;
+				rightHeight=right.height;
+			} else {
+				rightHeight=-1;
+			}
+			current.height=Math.max(leftHeight,rightHeight)+1;
 
+			//Check for imbalance, perform rotations if needed.
+
+			//TODO: Add height recalculation after rotation
+
+			//leftHeight >=0 prevent null dereference.
+			if(leftHeight>=0) {
+				//LL Rotation
+				if (leftHeight - rightHeight > 1 && x.compareTo(current.left.element) < 0){
+					//Check if root must be updated.
+					if(stack.peek()==null){
+						root=current.left;
+					}
+					Entry<T> temp=(Entry<T>) current.left.right;
+					current.left.right=current;
+					current.left=temp;
+				}
+				//TODO: LR Rotations
+			}
+			//TODO: RR and RL Rotations
+		}
 		return retValue;
     }
 
@@ -36,7 +78,6 @@ public class AVLTree<T extends Comparable<? super T>> extends BinarySearchTree<T
     public T remove(T x) {
         return super.remove(x);
     }
-
 
     /** TO DO
      *	verify if the tree is a valid AVL tree, that satisfies
